3b05c26fdf00dee527dc0042b1605388480411f8
rogger_christian <roggerc.dev@gmail.com>
2024-05-16 20:39:58 -0300

    Desafio completo, seguindo criterios propostos.

--- ContaBanco/src/ContaTermianal.java
+++ ContaBanco/src/ContaTermianal.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class ContaTermianal
  {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe scaner
+        //TODO: Exibir as mensagens para nosso usuario
+        //TODO: Exibir a mensagem final
+        int numero ;
+        String agencia;
+        String nomeCliente;
+        float saldo ;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Digite sua Agência:");
+        agencia = scanner.nextLine();
+        System.out.println("Digite o numero da conta - apenas números");
+        numero = scanner.nextInt();
+        System.out.println("Digite o saldo a depositar:");
+        saldo = scanner.nextFloat();
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia+", conta "+numero+" e seu saldo "+ saldo +"$ já está disponível para saque");
     }
 }
